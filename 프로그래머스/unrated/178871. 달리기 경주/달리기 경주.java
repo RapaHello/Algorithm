7dec1256315ceb12ecf70ac2d4459f749688a924
홍원표 <amuse1337@gmail.com>
2023-06-20 16:15:49 +0900

    [unrated] Title: 달리기 경주, Time: 0.10 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,9 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-        
+    public String[] solution(String[] players, String[] callings) {   
         Map<String, Integer> playerMap = new HashMap<>();
         if (players.length > 4) {
             for (int i = 0; i < players.length; i++) {
@@ -24,8 +22,7 @@ class Solution {
                 playerMap.put(players[idx - 1], idx - 1);
             }
         }
-        answer = players;
         
-        return answer;
+        return players;
     }
 }
