6b11c58ae2b1af7fee40eb16cb4fa6508a1ad163
홍원표 <amuse1337@gmail.com>
2023-06-21 09:26:08 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -13,15 +13,8 @@ class Solution {
             }
         }
 
-        Map<Integer, String[]> photoMap = new HashMap<>();
-        if (3 <= photo.length && photo.length <= 100) {
-            for (int i = 0; i < photo.length; i++) {
-                photoMap.put(i, photo[i]);
-            }
-        }
-
-        for (int i = 0; i < photoMap.size(); i++) {
-            String[] list = photoMap.get(i);
+        for (int i = 0; i < photo.length; i++) {
+            String[] list = photo[i];
             for (int j = 0; j < list.length; j++) {
                 if (nameScoreMap.containsKey(list[j])) {
                     answer[i] += nameScoreMap.get(list[j]);
