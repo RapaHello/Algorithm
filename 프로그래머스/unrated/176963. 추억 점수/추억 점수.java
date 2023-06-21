f59acb897fb8974b7ba6cd11bf98e746f3100a3d
홍원표 <amuse1337@gmail.com>
2023-06-21 09:42:52 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -1,21 +1,21 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        Map<String, Integer> map = IntStream.range(0, name.length)
-                .boxed()
-                .collect(Collectors.toMap(i -> name[i], i -> yearning[i], (a, b) -> b, HashMap::new));
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
 
-        int[] answer = new int[photo.length];
+
+        int answer[] = new int[photo.length];
         for (int i = 0; i < photo.length; i++) {
             for (int j = 0; j < photo[i].length; j++) {
-               answer[i] += map.getOrDefault(photo[i][j], 0);
+                answer[i] += map.getOrDefault(photo[i][j], 0);
             }
         }
-        
+
         return answer;
     }
 }
