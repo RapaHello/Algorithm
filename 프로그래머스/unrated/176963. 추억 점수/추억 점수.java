4a5462f8adb792648a61f9c41088c061e077f744
홍원표 <amuse1337@gmail.com>
2023-06-21 09:45:06 +0900

    Update 추억 점수.java

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -8,7 +8,6 @@ class Solution {
             map.put(name[i], yearning[i]);
         }
 
-
         int answer[] = new int[photo.length];
         for (int i = 0; i < photo.length; i++) {
             for (int j = 0; j < photo[i].length; j++) {
