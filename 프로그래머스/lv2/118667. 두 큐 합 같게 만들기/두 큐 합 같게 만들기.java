cedfe96afa12af047c3f2440a9eff980aa9959e0
홍원표 <amuse1337@gmail.com>
2023-06-29 09:27:32 +0900

    Update 두 큐 합 같게 만들기.java

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,34 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int ans = 0;
-		
-		long total=0;//두큐의합
-		long q1Sum=0;//1번큐의합
-		Queue<Integer> qq1 = new LinkedList<Integer>();
-		Queue<Integer> qq2 = new LinkedList<Integer>();
-		for(int i=0; i<queue1.length; i++) { 
-			total += queue1[i]+queue2[i];
-			q1Sum += queue1[i];
-			qq1.add(queue1[i]);
-			qq2.add(queue2[i]);
-		}
-		if(total%2!=0) return -1;//만약 두큐의합이 홀수면 같게 못만듦.
-		
-		long target = total/2;
-		while(true) {
-			if(ans>(queue1.length+queue2.length)*2) return -1;//이땐 더 순회해도 못만듦
-			if(q1Sum==target) break;
-			else if(q1Sum>target) {
-				q1Sum-=qq1.peek();
-				qq2.add(qq1.poll());
-			}else {
-				q1Sum += qq2.peek();
-				qq1.add(qq2.poll());
-			}
-			ans++;
-		}
-		
-		return ans;
+        int answer = 0;
+        int sum = 0, oSum = 0, pSum = 0;
+        Queue<Integer> oQueue = new LinkedList<>();
+        Queue<Integer> pQueue = new LinkedList<>();
+        for (int q1 : queue1) {
+            oQueue.add(q1);
+            oSum += q1;
+        }
+        for (int q2 : queue2) {
+            pQueue.add(q2);
+            pSum += q2;
+        }
+
+        sum = oSum + pSum;
+        if (sum % 2 == 1) {
+            return -1;
+        }
+
+        sum /= 2;
+        while (oSum != pSum) {
+            if (answer > queue1.length * 4) {
+                return -1;
+            }
+
+            if (oSum < sum) {
+                pSum -= pQueue.peek();
+                oSum += pQueue.peek();
+                oQueue.add(pQueue.poll());
+            } else {
+                pSum += oQueue.peek();
+                oSum -= oQueue.peek();
+                pQueue.add(oQueue.poll());
+            }
+
+            answer++;
+        }
+
+        return answer;
     }
 }
