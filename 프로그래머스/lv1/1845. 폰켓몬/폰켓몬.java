33aa5ba5c5128c731fa2330f400e265e7f330b1b
홍원표 <amuse1337@gmail.com>
2023-07-04 13:14:25 +0900

    [level 1] Title: 폰켓몬, Time: 4.21 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,13 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int num = nums.length / 2;
         HashSet<Integer> set = new HashSet<>();
         for (int i : nums) {
             set.add(i);
         }
-        
-        int result, size = set.size();
-        if (num <= size) {
-            result = num; 
-        } else {
-            result = size;
-        }
-                
+
+        int result = Math.min(set.size(), nums.length / 2);
+
         return result;
     }
 }
