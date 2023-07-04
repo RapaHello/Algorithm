71b2ba753a04513d9e588bbffc96730e3471053e
홍원표 <amuse1337@gmail.com>
2023-07-04 16:20:21 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -5,15 +5,11 @@ class Solution {
     public String solution(int a, int b) {
         Map<Integer, Integer> tempMap = new HashMap<>();
         String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
-        int[] month = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        for (int i = 0; i < 12; i++) {
-            tempMap.put(month[i], date[i]);
-        }
 
         int sum = b;
-        for (int i = 1; i < a; i++) {
-            sum += tempMap.get(i);
+        for (int i = 0; i < (a - 1); i++) {
+            sum += date[i];
         }
 
         int idx = sum % 7;
