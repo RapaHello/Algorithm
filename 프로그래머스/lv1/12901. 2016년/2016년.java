d68f6cf8ca00c63401b9833d13ac7e14a411ab6d
홍원표 <amuse1337@gmail.com>
2023-07-04 16:23:56 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,9 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public String solution(int a, int b) {
-        Map<Integer, Integer> tempMap = new HashMap<>();
         String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
         int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
